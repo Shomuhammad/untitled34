328e52a85d266816b852667bea5644ee1830ef23
Asus <Shomuhammad>
2022-01-19 16:40:31 +0600

    Initial commit

---
+++ src/com/company/LimitExcption.java
@@ -0,0 +1,13 @@
+package com.company;
+
+    public class LimitExcption extends Exception {
+        private double  remainingAmount;
+        public LimitExcption(String message, double remainingAmount) {
+            super(message);
+            this.remainingAmount = remainingAmount;
+        }
+        public double getRemainingAmount() {
+            return remainingAmount;
+        }
+    }
+
